4436aab0bd69e9410cf08e0e77a9c9e31be1d69a
javascripty <ed2711uribe@gmail.com>
2024-09-18 10:03:16 -0700

    Add blog page.

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className='max-w-[52ch] m-auto pt-24 mb-24'>
+      <p>Commenting System</p>
+      <p className='text-[#8C8C8D]'>
+        Some articles coming to a server near you
+      </p>
+    </main>
+  );
+}
